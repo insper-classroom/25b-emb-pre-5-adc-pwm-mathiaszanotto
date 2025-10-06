e85c74213f845889b6a562b563979fc65f435bd7
mathiaszanotto <81189943+mathiaszanotto@users.noreply.github.com>
2025-10-06 16:49:12 -0300

    exe3.1

--- exe3/main.c
+++ exe3/main.c
@@ -26,13 +26,32 @@ void data_task(void *p) {
 void process_task(void *p) {
     int data = 0;
 
+    static int buf[5] = {0};
+    static int idx = 0;
+    static int count = 0;
+    static int32_t acc = 0; 
+
     while (true) {
         if (xQueueReceive(xQueueData, &data, 100)) {
             // implementar filtro aqui!
+            if (count < 5) {
+                acc += data;
+                buf[idx] = data;
+                idx = (idx + 1) % 5;
+                count++;
+                if (count == 5) {
+                    int y = (int)(acc / 5);
+                    printf("%d \n", y);
+                }
+            } else {
+                acc -= buf[idx];
+                buf[idx] = data;
+                acc += data;
+                idx = (idx + 1) % 5;
 
-
-
-
+                int y = (int)(acc / 5);
+                printf("%d \n", y);
+            }
             // deixar esse delay!
             vTaskDelay(pdMS_TO_TICKS(50));
         }
